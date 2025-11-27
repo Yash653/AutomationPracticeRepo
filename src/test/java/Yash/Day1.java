900306fc3a9f9b16d0d840dc7ebf506c8b295c8a
Yash Kumar <yashk0083@gmail.com>
2025-11-27 13:21:32 +0530

    your commit message

--- src/test/java/Yash/Day1.java
+++ src/test/java/Yash/Day1.java
@@ -3,5 +3,6 @@ package Yash;
 public class Day1 {
  public static void main(String[] args){
      System.out.println("Hi i Am Yash");
+     System.out.print("I Am From Haryana, Ambala Cantt");
  }
 }
