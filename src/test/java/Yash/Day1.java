02bd779036debc4832330a500bf4ae9cc5442c6e
Yash Kumar <yashk0083@gmail.com>
2025-11-27 13:28:37 +0530

    Fifth Commit

--- src/test/java/Yash/Day1.java
+++ src/test/java/Yash/Day1.java
@@ -6,5 +6,7 @@ public class Day1 {
      System.out.print("I Am From Haryana, Ambala Cantt");
      System.out.println("My hoobies are : Cricket,Playing Video games");
      System.out.println("Fourth Commit");
+     System.out.println("Fifth Commit");
+     System.out.print("This is the Demo of Commits");
  }
 }
