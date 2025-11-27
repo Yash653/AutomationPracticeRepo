d56eb532abd9992873df5ba93e4df3f1288d5eab
Yash Kumar <yashk0083@gmail.com>
2025-11-27 13:25:39 +0530

    Third Commit

--- src/test/java/Yash/Day1.java
+++ src/test/java/Yash/Day1.java
@@ -4,5 +4,6 @@ public class Day1 {
  public static void main(String[] args){
      System.out.println("Hi i Am Yash");
      System.out.print("I Am From Haryana, Ambala Cantt");
+     System.out.println("My hoobies are : Cricket,Playing Video games");
  }
 }
